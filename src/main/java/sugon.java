02cf3ee3ff6f70089844eb9ad9f0c5fa33966afd
ekko-technology <joshyeo798@gmail.com>
2025-08-27 17:42:51 +0800

    Add level-2: Store text entered by user and display them back to user when requested

--- src/main/java/sugon.java
+++ src/main/java/sugon.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Sugon {
     public static void main(String[] args) {
@@ -8,14 +9,30 @@ public class Sugon {
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
         String line;
+        Scanner s = new Scanner(System.in);
+        ArrayList<String> list_of_commands = new ArrayList<>();
+
         while (true) {
-            Scanner s = new Scanner(System.in);
             line = s.nextLine();
             if (line.equals("bye"))
             {
                 break;
             }
-            System.out.println(line);
+            else if (line.equals("list")){
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < list_of_commands.size(); i++){
+
+                    System.out.println((i+1) + ". " + list_of_commands.get(i));
+                }
+                System.out.println("____________________________________________________________");
+
+            }
+            else {
+                list_of_commands.add(line);
+                System.out.println("____________________________________________________________");
+                System.out.println("Added: " + line);
+                System.out.println("____________________________________________________________");
+            }
         }
 
         System.out.println("____________________________________________________________");
