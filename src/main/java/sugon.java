397d6476a15c1a972e032c22647b468856663b77
ekko-technology <joshyeo798@gmail.com>
2025-08-27 17:04:44 +0800

    Add level-1: Implemented Echo and exits upon bye command

--- src/main/java/sugon.java
+++ src/main/java/sugon.java
@@ -1,9 +1,23 @@
-public class sugon {
+import java.util.Scanner;
+
+public class Sugon {
     public static void main(String[] args) {
         String chatName = "Sugon";
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm " + chatName);
         System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+        String line;
+        while (true) {
+            Scanner s = new Scanner(System.in);
+            line = s.nextLine();
+            if (line.equals("bye"))
+            {
+                break;
+            }
+            System.out.println(line);
+        }
+
         System.out.println("____________________________________________________________");
         System.out.println(" Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
